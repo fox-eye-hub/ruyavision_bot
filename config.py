ac616b11438415ee0481d524e9852f261d546ee2
fox-eye-hub <nzokirov2203@gmail.com>
2025-03-26 22:28:57 +0500

    Add files via upload

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 
-TELEGRAM_TOKEN = "SIZNING_BOT_TOKEN"
-ADMIN_ID = 123456789  # O‘zingizning Telegram ID'ingiz bilan almashtiring
+TELEGRAM_TOKEN = "7561865890:AAHZNzmiL2syVvBFEFVF1wXtv1G_vio2k14"
+ADMIN_ID =  7485117061 # O‘zingizning Telegram ID'ingiz bilan almashtiring
 GOOGLE_SHEET_NAME = "Buyurtmalar"
